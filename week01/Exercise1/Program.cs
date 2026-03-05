6ee7b7becb585c367d4bddc563f7833c8af74ead
Antoniozabala123 <lukinkeri051@gmail.com>
2026-03-05 00:50:21 -0400

    finished exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //ask the use for their name.
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        String last = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {first}, {first} {last}.");
     }
 }
