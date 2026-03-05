390146a2f28eba788402d210d929efed73e255e4
Antoniozabala123 <lukinkeri051@gmail.com>
2026-03-05 00:54:38 -0400

    add finished

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,13 +7,13 @@ class Program
     {
         //ask the use for their name.
         Console.WriteLine("What is your first name?");
-        string first = Console.ReadLine();
+        string First = Console.ReadLine();
 
         Console.Write("what is your last name? ");
-        String last = Console.ReadLine();
+        String Last = Console.ReadLine();
 
         Console.WriteLine();
 
-        Console.WriteLine($"Your name is {first}, {first} {last}.");
+        Console.WriteLine($"Your name is {First}, {First} {Last}.");
     }
 }
