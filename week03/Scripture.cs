123550483dc4ff7fb556a8f61ed0a0bc98526900
Antoniozabala123 <lukinkeri051@gmail.com>
2026-03-24 19:57:39 -0400

    add scriptures end

---
+++ week03/Scripture.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Scripture
+{
+    private string _References;
+
+    private string _Words;
+
+    public class Scripture(_References, _Words);
+    {
+        
+    }
+}
